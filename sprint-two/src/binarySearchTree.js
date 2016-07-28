885bec24c91cc6416db872a691d450e69b11b787
jon-is-learning <jonathan.garrett@gmail.com>
2016-07-28 15:41:42 -0700

    Finish B Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,56 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(val) {
+  var node = Object.create(bTreeMethods);
+  node.value = val;
+  node.left = undefined;
+  node.right = undefined;
+
+  return node;
 };
 
+var bTreeMethods = {};
+
+bTreeMethods.insert = function(val) {
+  if (val > this.value) {
+    if (this.right !== undefined) {
+      this.right.insert(val);
+    } else {
+      this.right = BinarySearchTree(val);
+    }
+  } else if (val < this.value) {
+    if (this.left !== undefined) {
+      this.left.insert(val);
+    } else {
+      this.left = BinarySearchTree(val);
+    }
+  }
+};  
+
+bTreeMethods.contains = function(val) {
+  var result = false;
+  if (val === this.value) {
+    result = true;
+  } else if (val < this.value && this.left !== undefined) {
+    result = this.left.contains(val);
+  } else if (val > this.value && this.right !== undefined) {
+    result = this.right.contains(val);
+  }
+  return result;
+};
+
+bTreeMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  //call callback on each value in the tree
+  //keep results, return results at the end
+  // debugger;
+
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(callback);
+  }
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(callback);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -55,7 +55,6 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for (var node in this.graphStorage) {
-    debugger;
     cb(this.graphStorage[node].value);
   }
 };

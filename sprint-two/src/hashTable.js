94370529ce9b9b1ba9aa33aa5b9c72410b8a0eaf
jon-is-learning <jonathan.garrett@gmail.com>
2016-07-28 10:45:01 -0700

    Finish hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,46 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this[index]) {
+    var vals = [[this[index], this[index]], [k, v]];
+    this[index] = vals;
+  } else {
+    this[index] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (Array.isArray(this[index])) {
+    var arrays = this[index];
+    for (var i = 0; i < arrays.length; i++) {
+      if (arrays[i][0] === k) {
+        return arrays[i][1];
+      }
+    }
+    return arrays[0][1];
+  }
+
+  return this[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (Array.isArray(this[index])) {
+    var arrays = this[index];
+    for (var i = 0; i < arrays.length; i++) {
+      if (arrays[i][0] === k) {
+        delete arrays[i][1];
+      }
+    }
+    delete arrays[0][1];
+  } else {
+    delete this[index];
+  }
+
 };
 
 
@@ -22,5 +54,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
